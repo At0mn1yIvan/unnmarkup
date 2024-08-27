943ab8b14036e4bb07f639f36d23a760f3d120f4
At0mn1yIvan <khramov.vanya@bk.ru>
2024-08-27 18:32:18 +0300

    Password Change System done

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 from users import views
+from users.views import PasswordChangeUser
 
 app_name = 'users'
 
@@ -8,5 +9,8 @@ urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterUser.as_view(), name='register'),
-    path('register-done/', views.register_user_done, name='register_done')
+    path('register/done/', views.register_user_done, name='register_done'),
+    path('profile/', views.ProfileUser.as_view(), name='profile'),
+    path('password-change/', PasswordChangeUser.as_view(), name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name='password_change_done'),
 ]
